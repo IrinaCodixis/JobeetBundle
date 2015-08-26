9490f41f9286800ac8877bab28afa75702f03d25
IrinaCodixis <ijumamuradova@codixis.com>
2015-08-26 16:43:52 +0300

    changed show.html.twig

--- Controller/CategoryController.php
+++ Controller/CategoryController.php
@@ -8,5 +8,20 @@ use Ens\JobeetBundle\Entity\Category;
 */
 class CategoryController extends Controller
 {
- 
+	public function showAction($slug)
+	{
+		$em = $this->getDoctrine()->getManager();
+	 
+		$category = $em->getRepository('EnsJobeetBundle:Category')->findOneBySlug($slug);
+	 
+		if (!$category) {
+			throw $this->createNotFoundException('Unable to find Category entity.');
+		}
+	 
+		$category->setActiveJobs($em->getRepository('EnsJobeetBundle:Job')->getActiveJobs($category->getId()));
+	 
+		return $this->render('EnsJobeetBundle:Category:show.html.twig', array(
+			'category' => $category,
+		));
+	}
 }
